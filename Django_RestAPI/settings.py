9cc9616b4e4a9fe2e220ab4d2929d29dd8a52bc9
Abhay <109977125+aghacker505@users.noreply.github.com>
2023-02-08 23:08:25 +0530

    database changed

--- Django_RestAPI/settings.py
+++ Django_RestAPI/settings.py
@@ -91,11 +91,17 @@ WSGI_APPLICATION = 'Django_RestAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'REST',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'3306',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        }
     }
 }
-
 
 
 # Password validation
