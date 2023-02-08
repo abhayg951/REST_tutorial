6f3c011cff809dcf7bc9b8c8457f08170f88f01d
Abhay <109977125+aghacker505@users.noreply.github.com>
2023-02-08 22:36:22 +0530

    updates

--- Django_RestAPI/settings.py
+++ Django_RestAPI/settings.py
@@ -89,12 +89,13 @@ WSGI_APPLICATION = 'Django_RestAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASES = {}
 
 
 # Password validation
